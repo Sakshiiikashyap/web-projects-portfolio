437a4019acb6100496fb0f3a77c935375fd88f40
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-20 17:41:10 +0530

    update

---
+++ paste-app/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:
+      <div>
+
+      </div>
+    },
+    {
+      path:"/pastes",
+      element:
+      <div>
+
+      </div>
+    },
+    {
+      path:"/pastes/:id",
+      element:
+      <div>
+
+      </div>
+    },
+
+  ]
+);
+
+function App() {
+  
+
+  return (
+    <div>
+      sakshi hello
+    </div>
+  )
+}
+
+export default App
