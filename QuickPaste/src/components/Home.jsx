eb5c962ca5d8d342de668399cd5e12cc9d81370d
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-23 21:13:16 +0530

    update

--- QuickPaste/src/components/Home.jsx
+++ QuickPaste/src/components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [title, setTitle] = useState('');
   return (
-    <div>Home</div>
+    <div>
+      <input
+      className='p-2 rounded-3xl'
+        type="text"
+        placeholder='enter title here'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button>
+        Create My Paste
+      </button>
+    </div>
   )
 }
 
--- QuickPaste/src/components/Navbar.jsx
+++ QuickPaste/src/components/Navbar.jsx
@@ -3,15 +3,45 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-       <NavLink>
+    <nav className="fixed top-0 left-0 w-full text-gray-100 z-50 shadow-[0_2px_8px_rgba(167,139,250,0.3)] backdrop-blur-sm bg-opacity-90"
+    style={{ backgroundColor: '#0c080d' }}
+    >
+    <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+      {/* <h1 className="text-2xl font-extrabold tracking-wide text-white">
+      <span className="text-violet-400 hover:text-violet-300 transition duration-300">
+        Quick
+      </span>
+      <span>Paste</span>
+      </h1> */}
+      <h1 className="text-2xl font-extrabold tracking-wide bg-gradient-to-r from-violet-500 to-pink-500 bg-clip-text text-transparent">
+  QuickPaste
+</h1>
+
+
+    <div className='flex gap-6 text-lg'>
+       <NavLink to="/"
+       className={({ isActive }) =>
+              isActive
+                ? 'text-violet-500 font-semibold underline underline-offset-4'
+                : 'text-gray-300 hover:text-pink-400 transition duration-300'
+            }
+       >
         Home
        </NavLink>
-       <NavLink>
+
+       <NavLink to="/pastes"
+       className={({ isActive }) =>
+              isActive
+                ? 'text-violet-500 font-semibold underline underline-offset-4'
+                : 'text-gray-300 hover:text-pink-400 transition duration-300'
+            }
+       >
         Pastes
        </NavLink>
     </div>
-  )
-}
+    </div>
+    </nav>
+  );
+};
 
 export default Navbar
