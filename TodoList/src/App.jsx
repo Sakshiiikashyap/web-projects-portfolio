abb69a410448f0d331c7871d1ea8fef3857830f7
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-08-25 15:54:15 +0530

    update

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -45,8 +45,10 @@ function App() {
   {/* Your Todos */}
   <h2 className="text-xl font-semibold mb-4 text-violet-800">Your Todos</h2>
   <div className="todos space-y-3">
-    <div className="todo flex justify-between items-center p-3 bg-white rounded-lg shadow-sm">
-      <div className="text-gray-700">My todo</div>
+    {todos.map(item=>{
+
+    return <div className="todo flex justify-between items-center p-3 bg-white rounded-lg shadow-sm">
+      <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
       <div className="buttons flex gap-2">
         <button onClick={handleEdit} className="px-4 py-1 text-sm bg-violet-600 text-white rounded-lg hover:bg-violet-700 transition">
           Edit
@@ -56,6 +58,7 @@ function App() {
         </button>
       </div>
     </div>
+    })}
   </div>
 </div>
 
